be53e428770940a3b308e1004a4c75a8db5bf5a0
lpbrown999 <lpbrown999@gmail.com>
2019-05-17 10:41:21 -0700

    Changing way plotting is done, adding results folder

--- plot_script.jl
+++ plot_script.jl
@@ -1,14 +1,50 @@
 using CSV
 using PyPlot
 
-filename = "result_400_100_3000_10.csv"
+
+
+#Load the file data
+filename = "results/result_400_100_3000_10.csv"
 file = CSV.read(filename,header=false,transpose=true)
 weights = Array(file.Column1)
 capacities = Array(file.Column2)
+SF = Array(file.Column3)
+δ = Array(file.Column4)
+
+#Grab the objectives we are trading off
+y1s = weights 
+y2s = -capacities
+ys = []
+
+
+#Sepearate into pareto optimal, non pareto optimal design points 
+for (y1,y2) in zip(y1s,y2s)
+	push!(ys,[y1,y2])
+end
+
+pareto_ys = []
+dominated_ys = []
+
+dominates(y, y′) = all(y′ - y .≥ 0) && any(y′ - y .> 0)
+for y in ys
+	if !any(dominates(y,y′) for y′ in ys) 
+		push!(pareto_ys,y)
+	else
+		push!(dominated_ys,y)
+	end
+end
 
 fig = figure("")
 ax = gca()
 ax.grid()
-ax.plot(weights,-capacities,"b*")
+
+for y in pareto_ys
+	ax.plot(y[1],y[2],"k*")
+end
+for y in dominated_ys
+	ax.plot(y[1],y[2],"r*")
+end
 ax.set(xlabel="Total Weight", ylabel="-Electrical Capacity", xticklabels=[], yticklabels=[])
-savefig("test.png",dpi=300)
+# savefig("test.png",dpi=300)
+
+
